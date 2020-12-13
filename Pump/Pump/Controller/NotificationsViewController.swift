142be9e435356cb234542d213fde77c62973eb8e
MoMoney14 <46688420+MoMoney14@users.noreply.github.com>
2020-12-13 15:41:24 -0600

    fixed notification

--- Pump/Pump/Controller/NotificationsViewController.swift
+++ Pump/Pump/Controller/NotificationsViewController.swift
@@ -11,12 +11,68 @@ import UIKit
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-class NotificationsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class NotificationsViewController: UIViewController {
     
     let db = Firestore.firestore()
     var notifications = [Notification]()
-
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        notifications = []
+        fetchNotifications()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //        if (segue.identifier == "toDetailedWorkout") {
+        //
+        //            let detailedPostView = segue.destination as? DetailedWorkoutController
+        //            detailedPostView?.postId = sender as! String
+        //        }
+        if(segue.identifier == "toDetailedWorkoutViewController") {
+            let detailedPostView = segue.destination as? DetailedPostViewController
+            detailedPostView?.postId = sender as? String
+            detailedPostView?.uniqueSegueIdentifier = "No Like Button"
+        }
+    }
+    
+    func fetchNotifications () {
+        DispatchQueue.global().async {
+            do {
+                var notiStruct: Notification?
+                let notificationRefs = self.db.collection("notifications")
+                let call = notificationRefs.whereField("receiverId", isEqualTo: userID)
+                call.getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("No results: \(err)")
+                    } else {
+                        for document in querySnapshot!.documents {
+                            try? notiStruct = document.data(as:Notification.self)
+                            self.notifications.append(notiStruct ?? Notification(postId: "err", postTitle: "err", receiverId: "err", senderId: "err"))
+                            //print (notiStruct ?? "unknown error")
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+extension NotificationsViewController: UITableViewDataSource, UITableViewDelegate{
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return notifications.count
@@ -47,61 +103,7 @@ class NotificationsViewController: UIViewController, UITableViewDataSource, UITa
         return myCell
     }
     
-    func setupTableView() {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: "toDetailedWorkoutViewController", sender: notifications[indexPath.row].postId)
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if (segue.identifier == "toDetailedWorkout") {
-//
-//            let detailedPostView = segue.destination as? DetailedWorkoutController
-//            detailedPostView?.postId = sender as! String
-//        }
-        if(segue.identifier == "toDetailedWorkoutViewController") {
-            let detailedPostView = segue.destination as? DetailedPostViewController
-            detailedPostView?.postId = sender as? String
-            detailedPostView?.uniqueSegueIdentifier = "No Like Button"
-        }
-    }
-    
-    
-    func fetchNotifications () {
-        DispatchQueue.global().async {
-            do {
-                var notiStruct: Notification?
-                let notificationRefs = self.db.collection("notifications")
-                let call = notificationRefs.whereField("receiverId", isEqualTo: userID)
-                call.getDocuments() { (querySnapshot, err) in
-                    if let err = err {
-                        print("No results: \(err)")
-                    } else {
-                        for document in querySnapshot!.documents {
-                            try? notiStruct = document.data(as:Notification.self)
-                            self.notifications.append(notiStruct ?? Notification(postId: "err", postTitle: "err", receiverId: "err", senderId: "err"))
-                            //print (notiStruct ?? "unknown error")
-                        }
-                    }
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                }
-            }
-        }
-    }
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.setupTableView()
-        fetchNotifications()
-    }
-    
 }
